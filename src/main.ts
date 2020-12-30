4abc0461bbe8a825e03b5ea641023dbf3bbf3e72
piyoppi <piyoppi@users.noreply.github.com>
2020-12-30 08:45:16 +0000

    deploy: eea9fbb54addfccf4170e654d44f041121e3966e

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { TiledMap, Projects, MapChipImage } from './../../src/main'
 
-const tiledMap = new TiledMap()
-tiledMap.initialize(30, 30, 32, 32)
+const tiledMap = new TiledMap(30, 30, 32, 32)
 tiledMap.mapChipsCollection.push(new MapChipImage("images/chip.png", 1))
 Projects.add(tiledMap, 1)
 
