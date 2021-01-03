3416efe7d19e18bf6d1c5cb8a3ae81ecad5ba993
piyoppi <garakuta.toolbox@gmail.com>
2021-01-03 21:39:25 +0900

    オートタイルをサンプルアプリにも導入する

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -2,7 +2,9 @@ import { TiledMap, Projects, MapChipImage } from './../../src/main'
 
 const tiledMap = new TiledMap(30, 30, 32, 32)
 tiledMap.mapChipsCollection.push(new MapChipImage("images/chip.png", 1))
+tiledMap.mapChipsCollection.push(new MapChipImage("images/auto-tile-sample.png", 2))
 Projects.add(tiledMap, 1)
 
 document.getElementById('mapChipSelector')?.setAttribute('projectId', '1')
+document.getElementById('autoTileSelector')?.setAttribute('projectId', '1')
 document.getElementById('mapCanvas')?.setAttribute('projectId', '1')
