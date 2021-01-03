790604d8923154306e15593ca463ae78954adc1f
piyoppi <piyoppi@users.noreply.github.com>
2021-01-03 12:57:56 +0000

    deploy: 15530bb45be1ef9f2f666e343446fae147c0cb56

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,29 @@ import { TiledMap, Projects, MapChipImage } from './../../src/main'
 
 const tiledMap = new TiledMap(30, 30, 32, 32)
 tiledMap.mapChipsCollection.push(new MapChipImage("images/chip.png", 1))
+tiledMap.mapChipsCollection.push(new MapChipImage("images/auto-tile-sample.png", 2))
 Projects.add(tiledMap, 1)
 
-document.getElementById('mapChipSelector')?.setAttribute('projectId', '1')
-document.getElementById('mapCanvas')?.setAttribute('projectId', '1')
+const mapChipSelector = document.getElementById('mapChipSelector')
+const autoTileSelector = document.getElementById('autoTileSelector')
+const mapCanvas = document.getElementById('mapCanvas')
+
+const init = () => {
+  if (!mapChipSelector || !autoTileSelector || !mapCanvas) return
+
+  mapChipSelector.setAttribute('projectId', '1')
+  autoTileSelector.setAttribute('projectId', '1')
+  mapCanvas.setAttribute('projectId', '1')
+
+  autoTileSelector.addEventListener('selected', () => {
+    mapCanvas.setAttribute('brush', 'RectangleBrush')
+    mapCanvas.setAttribute('arrangement', 'AutoTileArrangement')
+  })
+
+  mapChipSelector.addEventListener('selected', () => {
+    mapCanvas.setAttribute('brush', 'Pen')
+    mapCanvas.setAttribute('arrangement', 'DefaultArrangement')
+  })
+}
+
+init()
