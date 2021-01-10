bb6a893efab0198f953fb88cd1300b2b990c5818
piyoppi <garakuta.toolbox@gmail.com>
2021-01-10 18:17:35 +0900

    マップチップ周りのテストを追加

--- tests/MapChip.test.ts
+++ tests/MapChip.test.ts
@@ -15,3 +15,25 @@ describe('#compare', () => {
     expect(c1.compare(c2)).toEqual(false)
   });
 });
+
+describe('#clone', () => {
+  it('Return cloned item', () => {
+    const c1 = new MapChip(0, 0, 0)
+    const c2 = c1.clone()
+
+    expect(c1.compare(c2)).toEqual(true)
+  })
+})
+
+describe('#withParameter', () => {
+  it('Should set some parameters', () => {
+    const c1 = new MapChip(0, 0, 0)
+
+    c1.withParameter({x: 123, y: 456})
+    expect(c1.x).toEqual(123)
+    expect(c1.y).toEqual(456)
+
+    c1.withParameter({renderingArea: 3})
+    expect(c1.renderingArea).toEqual(3)
+  })
+})
