d8425f46a862da244c45859981df1a9e7f310dac
piyoppi <garakuta.toolbox@gmail.com>
2021-01-24 15:40:26 +0900

    一度に複数のオートタイルを消して再構築できるようにする

--- src/Brushes/Arrangements/AutoTileEraseArrangement.ts
+++ src/Brushes/Arrangements/AutoTileEraseArrangement.ts
@@ -24,47 +24,58 @@ export class AutoTileEraseArrangement implements Arrangement, TiledMapDataRequir
   apply(paints: Array<BrushPaint>): Array<BrushPaint> {
     if (paints.length === 0) return []
 
-    return this.erase(paints[0])
+    return this.erase(paints)
   }
 
-  erase(paint: BrushPaint): Array<BrushPaint> {
+  erase(paints: Array<BrushPaint>): Array<BrushPaint> {
     if (!this._tiledMapData) throw new Error('MapData is not set.')
 
     const resultPaints: Array<BrushPaint> = []
-    const x1 = Math.max(paint.x - 1, 0)
-    const y1 = Math.max(paint.y - 1, 0)
-    const x2 = Math.min(paint.x + 1, this._tiledMapData.width)
-    const y2 = Math.min(paint.y + 1, this._tiledMapData.height)
-    const patchWidth = x2 - x1 + 1
-    const patchHeight = y2 - y1 + 1
+
+    const paintX1 = paints.reduce((acc, val) => Math.min(acc, val.x), this._tiledMapData.width)
+    const paintY1 = paints.reduce((acc, val) => Math.min(acc, val.y), this._tiledMapData.height)
+    const paintX2 = paints.reduce((acc, val) => Math.max(acc, val.x), 0)
+    const paintY2 = paints.reduce((acc, val) => Math.max(acc, val.y), 0)
+
+    const x1 = Math.max(paintX1 - 1, 0)
+    const y1 = Math.max(paintY1 - 1, 0)
+    const x2 = Math.min(paintX2 + 1, this._tiledMapData.width)
+    const y2 = Math.min(paintY2 + 1, this._tiledMapData.height)
+
+    const bufferWidth = x2 - x1 + 1
+    const bufferHeight = y2 - y1 + 1
 
     const tiledBuffer = new TiledMapData(
-      patchWidth + 2,
-      patchHeight + 2
+      bufferWidth + 2,
+      bufferHeight + 2
     )
+    const bufferOffsetX = x1 - 1
+    const bufferOffsetY = y1 - 1
+    tiledBuffer.transferFromTiledMapData(this._tiledMapData, bufferOffsetX, bufferOffsetY, tiledBuffer.width, tiledBuffer.height, 0, 0)
 
-    const erasePosition = {x: 2, y: 2}
+    paints.forEach(paint => {
+      const paintPositionAtBuffer = {x: paint.x - bufferOffsetX, y: paint.y - bufferOffsetY}
 
-    tiledBuffer.transferFromTiledMapData(this._tiledMapData, x1 - 1, y1 - 1, tiledBuffer.width, tiledBuffer.height, 0, 0)
-    tiledBuffer.put(null, erasePosition.x, erasePosition.y)
-    resultPaints.push({x: paint.x, y: paint.y, chip: null})
+      tiledBuffer.put(null, paintPositionAtBuffer.x, paintPositionAtBuffer.y)
+      resultPaints.push({x: paint.x, y: paint.y, chip: null})
 
-    this._autoTileArrangement.setTiledMapData(tiledBuffer)
+      this._autoTileArrangement.setTiledMapData(tiledBuffer)
 
-    for (let y = 1; y < tiledBuffer.height - 1; y++) {
-      for (let x = 1; x < tiledBuffer.width - 1; x++) {
-        if (x === erasePosition.x && y === erasePosition.y) continue
+      for (let y = paintPositionAtBuffer.y - 1; y <= paintPositionAtBuffer.y + 1; y++) {
+        for (let x = paintPositionAtBuffer.x - 1; x <= paintPositionAtBuffer.x + 1; x++) {
+          if (x === paintPositionAtBuffer.x && y === paintPositionAtBuffer.y) continue
 
-        const chip = tiledBuffer.getMapDataFromChipPosition(x, y)
+          const chip = tiledBuffer.getMapDataFromChipPosition(x, y)
 
-        if (chip && chip instanceof AutoTileMapChip) {
-          this._autoTileArrangement.setAutoTile(chip.autoTile)
-          const appliedPaints = this._autoTileArrangement.apply([{x, y, chip}])
-          if (appliedPaints.length === 0) continue
-          resultPaints.push({x: appliedPaints[0].x + x1 - 1, y: appliedPaints[0].y + y1 - 1, chip: appliedPaints[0].chip})
+          if (chip && chip instanceof AutoTileMapChip) {
+            this._autoTileArrangement.setAutoTile(chip.autoTile)
+            const appliedPaints = this._autoTileArrangement.apply([{x, y, chip}])
+            if (appliedPaints.length === 0) continue
+            resultPaints.push({x: appliedPaints[0].x + bufferOffsetX, y: appliedPaints[0].y + bufferOffsetY, chip: appliedPaints[0].chip})
+          }
         }
       }
-    }
+    })
 
     return resultPaints
   }
