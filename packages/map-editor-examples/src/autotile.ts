ced56754cabd498d1183407d0219f4f9b5e0349f
piyoppi <garakuta.toolbox@gmail.com>
2021-02-16 01:02:16 +0900

    使っていない変数を消す

--- packages/map-editor-examples/src/autotile.ts
+++ packages/map-editor-examples/src/autotile.ts
@@ -4,8 +4,6 @@ import { TiledMap, MapChipImage, DefaultAutoTileImportStrategy } from '@piyoppi/
 const tiledMap = new TiledMap(30, 30, 32, 32)
 const mapChipImage = new MapChipImage("images/auto-tile-sample.png", 1)
 
-const mapCanvas = document.getElementById('mapCanvas') as HTMLElement
-
 const init = async () => {
   await mapChipImage.waitWhileLoading()
   tiledMap.mapChipsCollection.push(mapChipImage)
