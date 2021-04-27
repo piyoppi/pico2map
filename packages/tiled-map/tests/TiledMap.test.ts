c8458172afab8431b47d8220a94d743dbd52160a
piyoppi <garakuta.toolbox@gmail.com>
2021-04-27 21:38:07 +0900

    マップにリサイズメソッドを用意する

--- packages/tiled-map/tests/TiledMap.test.ts
+++ packages/tiled-map/tests/TiledMap.test.ts
@@ -1,13 +1,9 @@
 import { TiledMap } from './../src/TiledMap'
 import { MapChip, MapChipFragment } from './../src/MapChip'
 
-const mapSize = {
-  width: 10,
-  height: 15
-}
-const tiledMap = new TiledMap(mapSize.width, mapSize.height, 32, 32)
-
 describe('#convertChipPositionToPixel', () => {
+  const tiledMap = new TiledMap(5, 6, 32, 32)
+
   it('Should return converted position', () => {
     expect(tiledMap.convertChipPositionToPixel(0, 0)).toEqual({x: 0, y: 0})
     expect(tiledMap.convertChipPositionToPixel(2, 0)).toEqual({x: 64, y: 0})
@@ -17,6 +13,8 @@ describe('#convertChipPositionToPixel', () => {
 })
 
 describe('#put', () => {
+  const tiledMap = new TiledMap(5, 6, 32, 32)
+
   it('Should put a map chip', () => {
     const mapChip = new MapChip([new MapChipFragment(1, 1, 1)])
 
@@ -26,6 +24,12 @@ describe('#put', () => {
 })
 
 describe('data', () => {
+  const mapSize = {
+    width: 10,
+    height: 15
+  }
+  const tiledMap = new TiledMap(mapSize.width, mapSize.height, 32, 32)
+
   it('Should return map data', () => {
     const actualMapData = tiledMap.datas[0]
     expect(actualMapData.width).toEqual(mapSize.width)
@@ -34,6 +38,8 @@ describe('data', () => {
 })
 
 describe('#addLayer', () => {
+  const tiledMap = new TiledMap(5, 6, 32, 32)
+
   it('Should add a layer', () => {
     tiledMap.addLayer()
     expect(tiledMap.datas.length).toEqual(2)
@@ -43,6 +49,8 @@ describe('#addLayer', () => {
 })
 
 describe('#convertMapNumberToPosition', () => {
+  const tiledMap = new TiledMap(10, 15, 32, 32)
+
   it('Should return a position', () => {
     expect(tiledMap.convertMapNumberToPosition(0)).toEqual({x: 0, y: 0})
     expect(tiledMap.convertMapNumberToPosition(1)).toEqual({x: 1, y: 0})
@@ -51,5 +59,99 @@ describe('#convertMapNumberToPosition', () => {
     expect(tiledMap.convertMapNumberToPosition(11)).toEqual({x: 1, y: 1})
     expect(tiledMap.convertMapNumberToPosition(12)).toEqual({x: 2, y: 1})
     expect(tiledMap.convertMapNumberToPosition(149)).toEqual({x: 9, y: 14})
+  })
+})
+
+describe('#resize', () => {
+  const tiledMap = new TiledMap(4, 5, 32, 32)
+  const c1 = new MapChip([new MapChipFragment(1, 0, 0)])
+  const c2 = new MapChip([new MapChipFragment(2, 0, 0)])
+
+  beforeEach(() => {
+    tiledMap.addLayer()
+
+    // layer 0
+    tiledMap.put(c1, 0, 0, 0)
+    tiledMap.put(c1, 1, 0, 0)
+    tiledMap.put(c1, 2, 0, 0)
+    tiledMap.put(c2, 0, 2, 0)
+    tiledMap.put(c2, 1, 2, 0)
+    tiledMap.put(c2, 2, 2, 0)
+    // layer 1
+    tiledMap.put(c1, 1, 1, 1)
+    tiledMap.put(c1, 2, 1, 1)
+    tiledMap.put(c1, 3, 1, 1)
+    tiledMap.put(c2, 1, 3, 1)
+    tiledMap.put(c2, 2, 3, 1)
+    tiledMap.put(c2, 3, 3, 1)
+    //colider
+    tiledMap.coliders.put('colider', 0, 0)
+    tiledMap.coliders.put('colider', 1, 0)
+    tiledMap.coliders.put('colider', 2, 0)
+    tiledMap.coliders.put('colider', 0, 2)
+    tiledMap.coliders.put('colider', 1, 2)
+    tiledMap.coliders.put('colider', 2, 2)
+  })
+
+  it('Should map is expanded', () => {
+    tiledMap.resize(6, 7)
+
+    expect(tiledMap.chipCountX).toEqual(6)
+    expect(tiledMap.chipCountY).toEqual(7)
+
+    expect(tiledMap.datas[0].items).toEqual([
+        c1,   c1,   c1, null, null, null,
+      null, null, null, null, null, null,
+        c2,   c2,   c2, null, null, null,
+      null, null, null, null, null, null,
+      null, null, null, null, null, null,
+      null, null, null, null, null, null,
+      null, null, null, null, null, null
+    ])
+
+    expect(tiledMap.datas[1].items).toEqual([
+      null, null, null, null, null, null,
+      null,   c1,   c1,   c1, null, null,
+      null, null, null, null, null, null,
+      null,   c2,   c2,   c2, null, null,
+      null, null, null, null, null, null,
+      null, null, null, null, null, null,
+      null, null, null, null, null, null
+    ])
+
+    expect(tiledMap.coliders.items).toEqual([
+      'colider', 'colider', 'colider', 'none', 'none', 'none',
+         'none',    'none',    'none', 'none', 'none', 'none',
+      'colider', 'colider', 'colider', 'none', 'none', 'none',
+         'none',    'none',    'none', 'none', 'none', 'none',
+         'none',    'none',    'none', 'none', 'none', 'none',
+         'none',    'none',    'none', 'none', 'none', 'none',
+         'none',    'none',    'none', 'none', 'none', 'none'
+    ])
+  })
+
+  it('Should matrix is reduced', () => {
+    tiledMap.resize(2, 3)
+
+    expect(tiledMap.chipCountX).toEqual(2)
+    expect(tiledMap.chipCountY).toEqual(3)
+
+    expect(tiledMap.datas[0].items).toEqual([
+        c1,   c1,
+      null, null,
+        c2,   c2
+    ])
+
+    expect(tiledMap.datas[1].items).toEqual([
+      null, null,
+      null,   c1,
+      null, null
+    ])
+
+    expect(tiledMap.coliders.items).toEqual([
+      'colider', 'colider',
+         'none',    'none',
+      'colider', 'colider'
+    ])
   })
 })
