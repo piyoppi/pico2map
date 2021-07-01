ef5ae792b72382b6e0c6108c447fc4926a4506d4
piyoppi <garakuta.toolbox@gmail.com>
2021-07-02 00:32:43 +0900

    オートタイルが追加されたらAutoTileSelectorComponentに反映されるようにする

--- packages/map-editor/src/Injector.ts
+++ packages/map-editor/src/Injector.ts
@@ -2,9 +2,9 @@ export class Injector {
   inject(calledObject: any, method: (... _: any) => any, before: (() => void) | null, after: (() => void) | null) {
     const original = calledObject[method.name]
 
-    calledObject[method.name] = () => {
+    calledObject[method.name] = (...args: Array<any>) => {
       if (before) before()
-      const result = original.call(calledObject)
+      const result = original.apply(calledObject, args)
       if (after) after()
       return result
     }
