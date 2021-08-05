535aa7f600d2f7c990b0586e7e8488bfcfe4d20f
piyoppi <garakuta.toolbox@gmail.com>
2021-08-05 23:08:03 +0900

    DefaultAutoTileImportStrategyで範囲外の画像を読み込む不具合を修正する

--- packages/tiled-map/tests/MapChipImage.test.ts
+++ packages/tiled-map/tests/MapChipImage.test.ts
@@ -1,23 +1,5 @@
 import { MapChipImage } from '../src/MapChipImage'
-
-class DummyImage {
-  protected _onload = () => {}
-  protected _onerror = () => {}
-  protected _src = ''
-
-  constructor(_width?: number, _height?: number) {}
-
-  set src(value: string) {
-    this._src = value
-    this._onload()
-  }
-  get src() { return this._src }
-  set onload(value: () => void) { this._onload = value }
-  set onerror(value: () => void) { this._onerror = value }
-
-  get width() { return 100 }
-  get height() { return 128 }
-}
+import { DummyImage } from './Helpers/DummyImage'
 
 describe('waitWhileLoading', () => {
   it('Should return resolved promise when the image is loaded', () => {
